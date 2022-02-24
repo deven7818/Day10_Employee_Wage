647cc7939b9e9ac6cae5739bbc3a63f4af3f9999
deven7818 <deven7818@gmail.com>
2022-02-24 22:30:48 +0530

    Employee wage day 10

---
+++ EmpWageDay10/src/main/java/com/empwageday10/App.java
@@ -0,0 +1,9 @@
+package com.empwageday10;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation...!" );
+    }
+}
